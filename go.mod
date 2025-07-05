a39944fbc308de9d05687ef1466666573208a41a
sheeezh12 <awikwokgit71@gmail.com>
2025-07-05 21:21:52 +0700

    Versi awal, desain setengah jadi

---
+++ go.mod
@@ -0,0 +1,3 @@
+module randomquote
+
+go 1.24.2
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		http.Error(w, "ERROR", http.StatusInternalServerError)
+	}
+
+	data := map[string]string{
+		"id":     "1",
+		"author": "Lord Kanjeng",
+		"text":   "Jangan malas belajar",
+	}
+
+	tmpl.Execute(w, data)
+
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
+}
